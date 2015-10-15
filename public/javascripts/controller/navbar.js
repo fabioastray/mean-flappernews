29a2112390fd3a847d8281ef0c78c0b289932d28
fabio.campos <fabio.campos@provedatos.com>
2015-10-14 21:24:21 -0500

    + using $state

--- public/javascripts/controller/navbar.js
+++ public/javascripts/controller/navbar.js
@@ -1,12 +1,8 @@
 app.controller('navbar', [
-    '$scope', '$location', 'Auth',
-    function ($scope, $location, Auth) {
+    '$scope', 'Auth',
+    function ($scope, Auth) {
         
         init();
-        
-        $scope.go = function ( path ) {
-            $location.path( path );
-        };
         
         function init(){
             $scope.isLoggedIn = Auth.isLoggedIn;
